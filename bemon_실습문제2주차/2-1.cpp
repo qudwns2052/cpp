ddc2e0a1647fc7de4ef2c845256dcec9b110d280
최병준 <qudwns2052@gmail.com>
2019-02-11 19:14:10 +0900

    190211

---
+++ bemon_실습문제2주차/2-1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BaseArray
+{
+private:
+	int capacity; // 동적 할당된 메모리 용량
+	int *mem;
+
+protected:
+
+	BaseArray(int capacity = 100) 
+	{
+		this->capacity = capacity;
+		mem = new int[capacity];
+	}
+
+	~BaseArray() { delete[] mem; }
+	
+	void put(int index, int val) { mem[index] = val; }
+	
+	int get(int index) { return mem[index]; }
+	
+	int getCapacity() { return capacity; }
+};
+
+class MyQueue : public BaseArray
+{
+	int enidx;
+	int deidx;
+public:
+	MyQueue(int mQ) : BaseArray(mQ)
+	{
+		enidx = 0;
+		deidx = 0;
+	}
+
+	void enqueue(int n)
+	{
+		put(enidx++, n);
+	}
+
+	int capacity(void)
+	{
+		return getCapacity();
+	}
+
+	int length(void)
+	{
+		return (enidx - deidx);
+	}
+
+	int dequeue(void)
+	{
+		return get(deidx++);
+	}
+
+};
+
+int main(void) 
+{
+	MyQueue mQ(100);
+	int n;
+	cout << "큐에 삽입할 5개의 정수를 입력하라>> ";
+	for (int i = 0; i < 5; i++) 
+	{
+		cin >> n;
+		mQ.enqueue(n); // 큐에 삽입
+	}
+	cout << "큐의 용량:" << mQ.capacity() << ", 큐의 크기:" << mQ.length() << endl;
+	cout << "큐의 원소를 순서대로 제거하여 출력한다>> ";
+	while (mQ.length() != 0) 
+	{
+		cout << mQ.dequeue() << ' '; // 큐에서 제거하여 출력
+	}
+	cout << endl << "큐의 현재 크기 : " << mQ.length() << endl;
+
+	system("pause");
+
+	return 0;
+}
