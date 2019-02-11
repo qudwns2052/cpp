137faf15be8be3ef9aa27443d1e1c4121d7674bf
최병준 <qudwns2052@gmail.com>
2019-02-11 16:35:11 +0900

    190211

---
+++ bemon_question_19/19.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book 
+{
+	string title;
+	int price, pages;
+public:
+	Book(string title = "", int price = 0, int pages = 0) 
+	{
+		this->title = title; this->price = price; 	this->pages = pages;
+	}
+
+	bool operator<(Book & rhs)
+	{
+
+	}
+
+	void show() 
+	{
+		cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+	}
+	string getTitle() { return title; }
+};
+
+int main() 
+{
+	Book a("청춘", 20000, 300);
+	string b;
+	cout << "책 이름을 입력하세요>>";
+	getline(cin, b);
+	if (b < a)
+		cout << a.getTitle() << "이 " << b << "보다 뒤에 있구나!" << endl;
+
+	system("pause");
+
+	return 0;
+
+}
